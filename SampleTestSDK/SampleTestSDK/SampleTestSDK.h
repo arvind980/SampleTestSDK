8a82b9dbb7ed4c0513f5d1593c659c0724b7f311
Arvind Singh <arvindsingh@Arvinds-MacBook-Pro.local>
2022-10-16 11:24:16 +0530

    add framwork

---
+++ SampleTestSDK/SampleTestSDK/SampleTestSDK.h
@@ -0,0 +1,18 @@
+//
+//  SampleTestSDK.h
+//  SampleTestSDK
+//
+//  Created by Arvind Singh on 16/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleTestSDK.
+FOUNDATION_EXPORT double SampleTestSDKVersionNumber;
+
+//! Project version string for SampleTestSDK.
+FOUNDATION_EXPORT const unsigned char SampleTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleTestSDK/PublicHeader.h>
+
+
---
+++ SampleTestSDK/SampleTestSDK/Validate.swift
@@ -0,0 +1,16 @@
+//
+//  Validate.swift
+//  SampleSDk
+//
+//  Created by Arvind Singh on 15/10/22.
+//
+
+import Foundation
+
+public class Validate{
+    
+    public func fetchZone(_ zoneName: String) {
+       print("fetchZone = ",zoneName)
+    
+    }
+}
